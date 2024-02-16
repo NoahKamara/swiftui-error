b572b12c74eede586a51f8be60263669073f7ee8
Noah Kamara <mail@noahkamara.com>
2024-02-16 19:59:22 +0100

    chore: add SwiftUIErrorExample target to the package

--- Package.swift
+++ Package.swift
@@ -22,12 +22,11 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftUIError",
             plugins: []
         ),
+        .target(name: "SwiftUIErrorExample", dependencies: ["SwiftUIError"]),
         .testTarget(
             name: "SwiftUIErrorTests",
             dependencies: ["SwiftUIError"]),
