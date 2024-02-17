5f605145af3baef2ad8bf38f97b7ed75de27821e
Noah Kamara <mail@noahkamara.com>
2024-02-17 15:24:23 +0100

    feat: add support for alerting and handling errors in SwiftUI views

--- Sources/SwiftUIErrorExample/Example.swift
+++ Sources/SwiftUIErrorExample/Example.swift
@@ -57,11 +57,14 @@ struct ExampleView: View {
             ThrowingButton("BarError.fatal", action: { throw BarError.makeFatal() })
             
             // This is caught by the outter "Foo Error" onCatch
-            ThrowingButton("FooError", action: { throw BarError.makeFatal() })
+            ThrowingButton("FooError", action: { throw FooError() })
         }
         .onCatch(of: FooError.self, perform: { error in
             print("caught FooError in closure: ", error)
         })
+        .onCatch { error in
+            print("Any Error", error)
+        }
     }
 }
 
