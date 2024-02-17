3e74e582633d723e92f1a82bda9047206acb7e6d
Noah Kamara <mail@noahkamara.com>
2024-02-17 12:50:36 +0100

    feat: add support for async throwing tasks and error handling in SwiftUI views

---
+++ Sources/SwiftUIError/Interface/WithErrorHandling.swift
@@ -0,0 +1,31 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Noah Kamara on 17.02.24.
+//
+
+import SwiftUI
+
+public struct WithErrorHandling<Content: View>: View {
+    @Environment(\.errorHandling)
+    private var errorHandling
+    
+    let content: (ErrorHandling) -> Content
+    
+    public init(@ViewBuilder content: @escaping (ErrorHandling) -> Content) {
+        self.content = content
+    }
+    
+    public var body: some View {
+        content(errorHandling)
+    }
+}
+
+#Preview {
+    WithErrorHandling { errors in
+        ThrowingButton("Press Me") {
+            throw URLError(.cancelled)
+        }
+    }
+}
